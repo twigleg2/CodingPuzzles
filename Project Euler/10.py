6f4c35bf6c92d1ecccb883cee92110691ea39194
Gaven Finch <gavenfinch@gmail.com>
2025-05-07 10:14:14 -0600

    Create Utils; Add isPrime to utils

--- Project Euler/10.py
+++ Project Euler/10.py
@@ -1,13 +1,7 @@
 # Find the sum of all the primes below two million.
 
-import math
+from utils import isPrime
 MAX = 2_000_000
-
-def isPrime(num):
-    for r in range(2, math.floor(math.sqrt(num)) + 1):
-        if num % r == 0:
-            return False
-    return True
 
 primes = []
 for r in range(2, MAX):
