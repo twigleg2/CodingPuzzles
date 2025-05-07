5d593503fb71bd513875cee825689bd6c460f94c
Gaven Finch <gavenfinch@gmail.com>
2025-05-07 12:50:29 -0600

    Add extractDigits to utils

--- Project Euler/4.py
+++ Project Euler/4.py
@@ -1,19 +1,15 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+from utils import extractDigits
+
 def isPalindromicNumber(num):
-    digits = []
-    divisor = 10
-    while num > 0:
-        digits.append(num % divisor)
-        num //= divisor
+    digits = extractDigits(num)
     
     for r in range(len(digits)):
         if digits[r] != digits[-r-1]:
-            # print ("false: ", digits)
             return False
     
-    print("true: ", digits)
     return True
 
 palindrome = 0
