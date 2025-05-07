c3c6600a1e3231bc0111d79d60682714ffe20c49
Gaven Finch <gavenfinch@gmail.com>
2025-05-07 10:49:10 -0600

    Enhance isPrime utils function

--- Project Euler/utils.py
+++ Project Euler/utils.py
@@ -4,6 +4,9 @@ def helloWorld():
     print("Hello World!")
 
 def isPrime(num):
+    if num < 2:
+        return False
+    
     for r in range(2, math.floor(math.sqrt(num)) + 1):
         if num % r == 0:
             return False
