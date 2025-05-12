d9c73710f82fc7143f992aee823c08b1b8fa336b
Gaven Finch <gavenfinch@gmail.com>
2025-05-11 18:40:05 -0600

    Add nthTriangleNumber and charToAlphabetValue to utils

--- Project Euler/utils.py
+++ Project Euler/utils.py
@@ -1,5 +1,11 @@
 import math
 
+# charToAlphabetValue = {chr(i): i - 64 for i in range(65, 91)}  # A=1, B=2, ..., Z=26
+upperCharToAlphabetValue = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+# charToAlphabetValue = {chr(i): i - 96 for i in range(97, 123)}  # a=1, b=2, ..., z=26
+lowerCharToAlphabetValue = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+charToAlphabetValue = {**upperCharToAlphabetValue, **lowerCharToAlphabetValue}
+
 def helloWorld():
     print("Hello World!")
 
@@ -45,3 +51,6 @@ def isPalindromicNumber(num):
         if digits[r] != digits[-r-1]:
             return False
     return True
+
+def nthTriangleNumber(n):
+    return 0.5 * n * (n+1)
